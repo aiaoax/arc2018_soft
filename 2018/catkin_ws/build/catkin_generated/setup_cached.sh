55cdcb0134f718cf3b11ee598f2dbe0dd282dd55
ROS-Desktop <r@aiaoax.net>
2018-07-22 15:57:11 +0900

    ROS Add

---
+++ 2018/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH="/opt/ros/lunar/lib:/opt/ros/lunar/lib/x86_64-linux-gnu"
+export PWD="/home/ros/arc_osaka/2018/catkin_ws/build"
+export ROS_PACKAGE_PATH="/home/ros/arc_osaka/2018/catkin_ws/src:/opt/ros/lunar/share"
