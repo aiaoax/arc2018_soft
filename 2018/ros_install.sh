2aa43993c4f66f7b0efd27428a2c82810aa11e47
Ryo Tabuchi <tabuchi.ryo0526@gmail.com>
2018-10-01 01:29:09 +0900

    catkin_ws→arc_wsに変更したため ./~bashrcのパスも更新

--- 2018/ros_install.sh
+++ 2018/ros_install.sh
@@ -1,4 +1,3 @@
-
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' &&
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116 &&
 sudo apt update &&
@@ -11,12 +10,12 @@ echo "source /opt/ros/lunar/setup.bash" >> ~/.bashrc &&
 source ~/.bashrc &&
 sudo apt -y install python-rosinstall python-rosinstall-generator python-wstool build-essential ros-lunar-navigation ros-lunar-viz ros-lunar-gazebo-ros-pkgs ros-lunar-gazebo-ros-control git &&
 
-echo "source ~/arc_osaka/2018/catkin_ws/devel/setup.bash" >> ~/.bashrc &&
+echo "source ~/arc_osaka/2018/arc_ws/devel/setup.bash" >> ~/.bashrc &&
 echo "export ROS_HOSTNAME=127.0.0.1" >> ~/.bashrc &&
 echo "export ROS_MASTER_URI=http://\${ROS_HOSTNAME}:11311" >> ~/.bashrc &&
-echo "alias cw='cd ~/arc_osaka/2018/catkin_ws'" >> ~/.bashrc &&
-echo "alias cs='cd ~/arc_osaka/2018/catkin_ws/src'" >> ~/.bashrc &&
-echo "alias cm='cd ~/arc_osaka/2018/catkin_ws && catkin_make'" >> ~/.bashrc &&
+echo "alias cw='cd ~/arc_osaka/2018/arc_ws'" >> ~/.bashrc &&
+echo "alias cs='cd ~/arc_osaka/2018/arc_ws/src'" >> ~/.bashrc &&
+echo "alias cm='cd ~/arc_osaka/2018/arc_ws && catkin_make'" >> ~/.bashrc &&
 
 cd &&
 
