6de482c4a3c466edd84d53a9fcfe5e8e7d4b89d7
天野陽直 <akno@live.jp>
2018-10-06 02:51:49 +0900

    arm.py 試し書き

--- 2018/arc_ws/src/tama/scripts/arm.py
+++ 2018/arc_ws/src/tama/scripts/arm.py
@@ -9,10 +9,19 @@ import rospy
 from tama.msg import arm
 
 # defined const
-PIN_SARVO1 = 7
-PIN_SARVO2 = 15
-PIN_INCW = 16
-PIN_INCCW = 1
+
+# pin number
+PIN_SARVO1 = 4
+PIN_SARVO2 = 14
+PIN_INCW = 15
+PIN_INCCW = 18
+
+
+CW_SARVO1 = 1500
+CW_SARVO2 = 180
+CCW_SARVO1 = 1000
+CCW_SARVO2 = 0
+
 
 # initialize gpio
 pi = pigpio.pi()
@@ -24,7 +33,7 @@ pi.set_mode(PIN_INCCW, pigpio.OUTPUT)
 def callback(arm):
     duty = ((arm.frame_id % 90.) / 180. * 1.9 % 0.5)\
             / 20. * 1e6
-    pi.hardware_PWM(pwm_pin, 50, 50000)
+    #pi.hardware_PWM(pwm_pin, 50, 50000)
     print ('frame_id = %d ' % arm.frame_id )
     print ("strike = %s" %  arm.strike )
     print("grub = %s" % arm.grub )
@@ -34,6 +43,20 @@ def callback(arm):
     print("updown = %s" % arm.updown )
     print("release = %s" % arm.release)
     print("=============")
+    
+    if arm.strike:
+        pi.write(PIN_INCW,1)
+    else:
+        pi.write(PIN_INCW,0)
+    
+    if arm.grub:
+        pi.set_servo_pulsewidth(PIN_SARVO1, CW_SARVO1)
+    else:
+        pi.set_servo_pulsewidth(PIN_SARVO1, CCW_SARVO1)
+    
+    
+
+
 
 def arm_py():
     rospy.init_node('arm_py_node',anonymous=True)
