1c47488b431ff5dd32e5e7562b46c26dff1f2c58
greatjihoon <thth5545@gmail.com>
2018-10-08 12:04:37 +0900

    空白調整 foot.py

--- 2018/arc_ws/src/tama/scripts/foot.py
+++ 2018/arc_ws/src/tama/scripts/foot.py
@@ -20,9 +20,9 @@ PIN_BIN1    = 20    # GPIO.28 Right IN1
 PIN_BIN2    = 16    # GPIO.27 Right IN2
 PIN_PWMB    = 12    # GPIO.26 Right PWM
 
-HIGH_SPD     = 255   # 速度：高, 値の範囲：0-255
-MEDIUM_SPD   = 192   # 速度：中, 値の範囲：0-255
-LOW_SPD      = 128   # 速度：低, 値の範囲：0-255
+HIGH_SPD    = 255   # 速度：高, 値の範囲：0-255
+MEDIUM_SPD  = 192   # 速度：中, 値の範囲：0-255
+LOW_SPD     = 128   # 速度：低, 値の範囲：0-255
 
 HIGH        = 1     # 定数
 LOW         = 0     # 定数
@@ -49,31 +49,31 @@ def callback(foot):
     #方向制御
     #前進
     if foot.direction == Direction.AHEAD:
-        outputDirection(LOW, HIGH, HIGH, LOW)      # Left Motor : CCW, Right Motor : CW
+        outputDirection(LOW, HIGH, HIGH, LOW)   # Left Motor : CCW, Right Motor : CW
     else:
         pass
 
     #後進
     if foot.direction == Direction.BACK:
-        outputDirection(HIGH, LOW, LOW, HIGH)      # Left Motor : CW, Right Motor : CCW
+        outputDirection(HIGH, LOW, LOW, HIGH)   # Left Motor : CW, Right Motor : CCW
     else:
         pass
 
     #右旋回
     if foot.direction == Direction.RIGHT:
-        outputDirection(LOW, HIGH, LOW, HIGH)      # Left Motor : CCW, Right Motor : CCW
+        outputDirection(LOW, HIGH, LOW, HIGH)   # Left Motor : CCW, Right Motor : CCW
     else:
         pass
 
     #左旋回
     if foot.direction == Direction.LEFT:
-        outputDirection(HIGH, LOW, HIGH, LOW)      # Left Motor : CW, Right Motor : CW
+        outputDirection(HIGH, LOW, HIGH, LOW)   # Left Motor : CW, Right Motor : CW
     else:
         pass
 
     #停止
     if foot.direction == Direction.STOP:
-        outputDirection(HIGH, HIGH, HIGH, HIGH)    # Left Motor : ShortBreak, Right Motor : ShortBreak
+        outputDirection(HIGH, HIGH, HIGH, HIGH) # Left Motor : ShortBreak, Right Motor : ShortBreak
     else:
         pass
 
@@ -91,11 +91,11 @@ def callback(foot):
     else:
         pass
 
-def outputPwm(SPD):                        # PWM Duty比
+def outputPwm(SPD):                     # PWM Duty比
     pi.set_PWM_dutycycle(PIN_PWMA, SPD)
     pi.set_PWM_dutycycle(PIN_PWMB, SPD)
 
-def outputDirection(AIN1, AIN2, BIN1, BIN2):   # 方向
+def outputDirection(AIN1, AIN2, BIN1, BIN2):    # 方向
     #Left Motor
     pi.write(PIN_AIN1,AIN1)
     pi.write(PIN_AIN2,AIN2)
